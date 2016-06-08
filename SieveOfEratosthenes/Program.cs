57772c32c5452e9ba35e91b23ae9580cd4a895a1
Hussen Fakhrulddin <hf556482@gmail.com>
2016-06-07 21:44:47 -0700

    init

---
+++ SieveOfEratosthenes/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SieveOfEratosthenes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        // Build a Sieve of Eratosthenes.
+        private bool[] MakeSieve(int max)
+        {
+            // Make an array indicating whether numbers are prime.
+            bool[] is_prime = new bool[max + 1];
+            for (int i = 2; i <= max; i++) is_prime[i] = true;
+
+            // Cross out multiples.
+            for (int i = 2; i <= max; i++)
+            {
+                // See if i is prime.
+                if (is_prime[i])
+                {
+                    // Knock out multiples of i.
+                    for (int j = i * 2; j <= max; j += i)
+                        is_prime[j] = false;
+                }
+            }
+            return is_prime;
+        }
+    }
+}
